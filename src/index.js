6713977a0da77404bd763f87a96b5773d9a15872
Tanishq <sharmatanishq0308@gmail.com>
2024-12-19 18:50:24 +0530

    mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import { app } from './app.js';
 
 dotenv.config({
     path:'./env'
 });
 
-connectDB();
+connectDB()
+.then(
+    app.on("error",(err)=>{
+        console.log("Error found: ",err);
+    }),
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App is listing on: ${process.env.PORT}`);
+    })
+)
+.catch((err)=>{
+    console.log("db connection failed: ", err);
+    
+})
 
 
 // import express from 'express'
